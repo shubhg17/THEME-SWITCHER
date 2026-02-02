bac29e392afa745f51289256105109dbea37b148
shubhg17 <shubamghai116@gmail.com>
2026-02-03 02:02:33 +0530

    Added Dark Mode in Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  
+  darkMode: "class", // 👈 THIS LINE
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
 
